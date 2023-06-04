0f5d0087e96d73a5417e8b52a4e5b60fab984717
yopa <90709631+yopaaa@users.noreply.github.com>
2023-06-04 20:48:43 +0700

    change variable address extern  to #defind

--- lib/Var/Var.h
+++ lib/Var/Var.h
@@ -11,24 +11,27 @@
 #define NUM_DOTS 2
 
 #define LED_PIN 23    // Seven segment pin
-#define DOTS_PIN 13   // Dots pin
 #define WIFI_AP_BTN 4 // Start wifi mode AP pin button
 
 #define CODE_VERSION "v3.5.25" // tahun 202'3', bulan .., tanggal ..
+
+#define SSID_ADDRESS 400
+#define PASSWORD_ADDRESS 415
+
+#define RED_ADDRESS 101
+#define GREEN_ADDRESS 102
+#define BLUE_ADDRESS 103
+#define COLOR_MODE_ADDRESS 105
+
+#define TIME_FORMAT_ADDRESS 120
+#define IP_ADDRESS_START_ADDRESS 200
+#define IS_STATIC_IP_ADDRESS 205
+
 extern String ssid;
 extern String password;
 
 extern const char *APssid;
 extern const char *APpassword;
-
-extern const int SSID_ADDRESS;
-extern const int PASSWORD_ADDRESS;
-extern const int RED_ADDRESS;
-extern const int GREEN_ADDRESS;
-extern const int BLUE_ADDRESS;
-extern const int TIME_FORMAT_ADDRESS;
-extern const int IP_ADDRESS_START_ADDRESS;
-extern const int IS_STATIC_IP_ADDRESS;
 
 extern const char *ntpServer;
 extern const long gmtOffset_sec; // UTC+7
