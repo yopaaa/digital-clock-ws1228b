d824d54814229e7e617940a8eb25e8b1d0015d08
yopa <90709631+yopaaa@users.noreply.github.com>
2023-06-04 20:47:38 +0700

    remove address variables

--- lib/Var/Var.cpp
+++ lib/Var/Var.cpp
@@ -6,17 +6,6 @@ String password = "";
 const char *APssid = "digital_clock";
 const char *APpassword = "12345678zxcvbnm";
 
-const int SSID_ADDRESS = 400;
-const int PASSWORD_ADDRESS = 415;
-
-const int RED_ADDRESS = 101;
-const int GREEN_ADDRESS = 102;
-const int BLUE_ADDRESS = 103;
-
-const int TIME_FORMAT_ADDRESS = 120;
-const int IP_ADDRESS_START_ADDRESS = 200;
-const int IS_STATIC_IP_ADDRESS = 205;
-
 const char *ntpServer = "pool.ntp.org";
 const long gmtOffset_sec = 7 * 3600; // UTC+7
 const int daylightOffset_sec = 0;
