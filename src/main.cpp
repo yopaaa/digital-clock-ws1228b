3b1a0c5fb50008da28ec3a5541ac519070783870
yopa <90709631+yopaaa@users.noreply.github.com>
2023-06-05 17:31:31 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,21 +7,87 @@
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
   setupEEPROM();
   Wire.begin();
   setupLED();
-  pinMode(WIFI_AP_BTN, INPUT_PULLUP);
+  pinMode(WIFI_AP_BTN, INPUT_PULLDOWN);
   TestStartUp();
   readWifiCredentials();
 
-  startWifiSta(); // STA WIFI
+  digitalRead(WIFI_AP_BTN) == HIGH ? startWifiAp() : startWifiSta();
   httpHandler();
+}
+
+void processCommand(const String &command)
+{
+  // Lakukan tindakan atau tanggapan terkait dengan perintah
+  Serial.print("Perintah diterima: ");
+  Serial.println(command);
 }
 
 // LOOP FUNCTION
 void loop()
 {
+  // if (Serial.available())
+  // {
+  //   String command = Serial.readStringUntil('\n');
+
+  //   Serial.print("-> ");
+  //   Serial.println(command);
+  //   if (command == "start server")
+  //   {
+  //     startServer();
+  //   }
+  //   else if (command == "test")
+  //   {
+  //     Serial.print("|");
+  //     Serial.print("\b");
+  //     delay(100);
+  //     Serial.print("/");
+  //     Serial.print("\b");
+  //     delay(100);
+  //     Serial.print("-");
+  //     Serial.print("\b");
+  //     delay(100);
+  //     Serial.print("/");
+  //     Serial.print("\b");
+  //     delay(100);
+  //     Serial.print("-");
+  //     Serial.print("\b");
+  //     delay(100);
+  //     Serial.print("\\");
+  //     Serial.print("\b");
+  //     delay(100);
+  //   }
+
+  // }
+  static String command;
+  Serial.print("\r");    // atau \x1B[K
+  Serial.print("-> ");   // atau \x1B[K
+  Serial.print(command); // atau \x1B[K
+
+  while (Serial.available())
+  {
+
+    char c = Serial.read();
+
+    if (c == '\n')
+    {
+      if (command.length() > 0)
+      {
+        processCommand(command);
+      }
+
+      command = "";
+    }
+    else if (isPrintable(c))
+    {
+      command += c;
+    }
+  }
+
+  // Serial.println("no command detect");
   if (displayMode == "clock")
   {
     printLocalTime();
@@ -36,5 +102,9 @@ void loop()
   {
     printCountDown();
     delay(1000);
+  }
+  else
+  {
+    ShowDotsRgb(0, 0, 255);
   }
 }
