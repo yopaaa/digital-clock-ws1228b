c8d941d2186e2581a9691a4ce29e212c345db38b
yopa <90709631+yopaaa@users.noreply.github.com>
2023-06-07 13:33:41 +0700

    Update Var.cpp

--- lib/Var/Var.cpp
+++ lib/Var/Var.cpp
@@ -1,14 +1,15 @@
 #include "Var.h"
+#include "Led.h"
 
 String ssid = "";
 String password = "";
 
-const char *APssid = "digital_clock";
-const char *APpassword = "12345678zxcvbnm";
+char *APssid = "digital_clock";
+char *APpassword = "12345678zxcvbnm";
 
-const char *ntpServer = "pool.ntp.org";
-const long gmtOffset_sec = 7 * 3600; // UTC+7
-const int daylightOffset_sec = 0;
+char *ntpServer = "pool.ntp.org";
+long gmtOffset_sec = 7 * 3600; // UTC+7
+int daylightOffset_sec = 0;
 
 byte RED = 255;
 byte GREEN = 0;
@@ -17,7 +18,7 @@ byte BRIGHTNESS = 100;
 
 String ColorMode = "";
 
-String displayMode = "clock";
+String Mode = "clock";
 int RefreshDelay = 1000;
 int counterCount = 0;
 int counterLimit = 120;
@@ -32,3 +33,5 @@ IPAddress IP(0, 0, 0, 0);
 IPAddress Gateway(0, 0, 0, 0);
 IPAddress Subnet(0, 0, 0, 0);
 IPAddress DNS1(0, 0, 0, 0);
+
+void (*mainLoop)() = &printLocalTime;
