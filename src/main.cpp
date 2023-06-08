5b54338b6540198cd37324ee925c45d7b78e993a
yopa <90709631+yopaaa@users.noreply.github.com>
2023-06-08 18:31:19 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -11,100 +11,25 @@ void setup()
   setupEEPROM();
   Wire.begin();
   setupLED();
+#if defined(ESP32)
   pinMode(WIFI_AP_BTN, INPUT_PULLDOWN);
+#elif defined(ESP8266)
+  pinMode(WIFI_AP_BTN, INPUT_PULLDOWN_16);
+#endif
   TestStartUp();
   readWifiCredentials();
-
   digitalRead(WIFI_AP_BTN) == HIGH ? startWifiAp() : startWifiSta();
   httpHandler();
 }
 
-void processCommand(const String &command)
-{
-  // Lakukan tindakan atau tanggapan terkait dengan perintah
-  Serial.print("Perintah diterima: ");
-  Serial.println(command);
-}
-
-// LOOP FUNCTION
 void loop()
 {
-  // if (Serial.available())
-  // {
-  //   String command = Serial.readStringUntil('\n');
-
-  //   Serial.print("-> ");
-  //   Serial.println(command);
-  //   if (command == "start server")
-  //   {
-  //     startServer();
-  //   }
-  //   else if (command == "test")
-  //   {
-  //     Serial.print("|");
-  //     Serial.print("\b");
-  //     delay(100);
-  //     Serial.print("/");
-  //     Serial.print("\b");
-  //     delay(100);
-  //     Serial.print("-");
-  //     Serial.print("\b");
-  //     delay(100);
-  //     Serial.print("/");
-  //     Serial.print("\b");
-  //     delay(100);
-  //     Serial.print("-");
-  //     Serial.print("\b");
-  //     delay(100);
-  //     Serial.print("\\");
-  //     Serial.print("\b");
-  //     delay(100);
-  //   }
-
-  // }
-  static String command;
-  Serial.print("\r");    // atau \x1B[K
-  Serial.print("-> ");   // atau \x1B[K
-  Serial.print(command); // atau \x1B[K
-
-  while (Serial.available())
+  if (ColorMode == "random")
   {
-
-    char c = Serial.read();
-
-    if (c == '\n')
-    {
-      if (command.length() > 0)
-      {
-        processCommand(command);
-      }
-
-      command = "";
-    }
-    else if (isPrintable(c))
-    {
-      command += c;
-    }
+    RED = random(1, 255);
+    GREEN = random(1, 255);
+    BLUE = random(1, 255);
   }
 
-  // Serial.println("no command detect");
-  if (displayMode == "clock")
-  {
-    printLocalTime();
-    delay(RefreshDelay);
-  }
-  else if (displayMode == "counter")
-  {
-    printCounter();
-    delay(1000);
-  }
-  else if (displayMode == "countdown")
-  {
-    printCountDown();
-    delay(1000);
-  }
-  else
-  {
-    ShowDotsRgb(0, 0, 255);
-  }
+  mainLoop();
 }
