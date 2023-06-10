98f6ed524d6990db544b920e96d64cc1e8da0296
yopa <90709631+yopaaa@users.noreply.github.com>
2023-06-10 20:08:05 +0700

    Create Buzzer.h

---
+++ lib/Buzzer/Buzzer.h
@@ -0,0 +1,21 @@
+#ifndef BUZZER_H
+#define BUZZER_H
+
+#include <Arduino.h>
+#include "Var.h"
+
+class Buzzer
+{
+private:
+    int pin;
+
+public:
+    Buzzer(int pin);
+    void on();
+    void off();
+    void beep(int timeout);
+};
+
+extern Buzzer myBuzzer;
+
+#endif
