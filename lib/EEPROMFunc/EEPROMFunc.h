da818ea6707b00dc1fff09ef73f8b9f043b0021a
yopa <90709631+yopaaa@users.noreply.github.com>
2023-06-11 19:03:03 +0700

    Update EEPROMFunc.h

--- lib/EEPROMFunc/EEPROMFunc.h
+++ lib/EEPROMFunc/EEPROMFunc.h
@@ -2,17 +2,23 @@
 #define EEPROM_FUNC
 
 #include <Arduino.h>
+#include "Var.h"
 
 void setupEEPROM();
 
-void defaultState();
 void readWifiCredentials();
 
 void readColor();
+void resetEEPROM();
+bool isEepromNotEmpty(int address);
+void write(int address, int value);
 void writeStaticIp(const IPAddress &ip);
 void writeString(int address, String value);
 void writeBool(int address, bool value);
 void writeLong(int address, long value);
 String readString(int address);
+bool readBool(int address);
+long readLong(int address);
+byte readByte(int address);
 
 #endif
