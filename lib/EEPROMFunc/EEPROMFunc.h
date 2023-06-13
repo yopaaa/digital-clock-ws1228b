eec97ee8051abfcd6aedc0f89ed286982693a5c0
yopa <90709631+yopaaa@users.noreply.github.com>
2023-06-13 21:20:44 +0700

    Update EEPROMFunc.h

--- lib/EEPROMFunc/EEPROMFunc.h
+++ lib/EEPROMFunc/EEPROMFunc.h
@@ -5,20 +5,27 @@
 #include "Var.h"
 
 void setupEEPROM();
-
 void readWifiCredentials();
-
 void readColor();
-void resetEEPROM();
-bool isEepromNotEmpty(int address);
-void write(int address, int value);
 void writeStaticIp(const IPAddress &ip);
+
+void beginEEPROM(int size);
+void resetEEPROM();
+
+bool isEepromNotEmpty(int address);
+
+void write(int address, int value);
+int read(int address);
+
 void writeString(int address, String value);
-void writeBool(int address, bool value);
-void writeLong(int address, long value);
 String readString(int address);
+
+void writeBool(int address, bool value);
 bool readBool(int address);
+
+void writeLong(int address, long value);
 long readLong(int address);
+
 byte readByte(int address);
 
 #endif
