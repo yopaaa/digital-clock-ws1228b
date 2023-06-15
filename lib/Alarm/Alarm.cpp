a5c2a6daed074f24c56b1dab38609dc0ac9a8eb2
yopa <90709631+yopaaa@users.noreply.github.com>
2023-06-15 20:34:42 +0700

    Create Alarm.cpp

---
+++ lib/Alarm/Alarm.cpp
@@ -0,0 +1,102 @@
+#include <EEPROM.h>
+#include <Var.h>
+#include "Alarm.h"
+#include "Buzzer.h"
+
+Alarm alarms[MAX_ALARMS] = {};
+
+// Fungsi untuk menulis data alarm ke EEPROM
+void writeAlarmsToEEPROM()
+{
+    int address = ALARM_LIST_ADDRESS;
+
+    for (int i = 0; i < MAX_ALARMS; i++)
+    {
+        Serial.print("\nwriting alarms ");
+        Serial.print(i);
+        Serial.print(" to ");
+        Serial.print(address);
+
+        EEPROM.write(address, alarms[i].hour);
+        EEPROM.write(address + 1, alarms[i].min);
+        EEPROM.write(address + 2, alarms[i].alarmDelay / 100);
+        address += 3;
+    }
+    EEPROM.commit();
+}
+
+// Fungsi untuk membaca data alarm dari EEPROM
+void readAlarmsFromEEPROM()
+{
+    int address = ALARM_LIST_ADDRESS;
+
+    for (int i = 0; i < MAX_ALARMS; i++)
+    {
+        Serial.print("\nreading alarms ");
+        Serial.print(i);
+        Serial.print(" to ");
+        Serial.print(address);
+
+        alarms[i].hour = EEPROM.read(address);
+        alarms[i].min = EEPROM.read(address + 1);
+        alarms[i].alarmDelay = EEPROM.read(address + 2) * 100;
+        address += 3;
+    }
+}
+
+// Fungsi untuk mengatur alarm pada indeks tertentu
+void setAlarm(int index, int hour, int min, int alarmDelay)
+{
+    Serial.print("\nset alarms ");
+    Serial.print(index);
+    Serial.print(" to hour");
+    Serial.print(hour);
+
+    alarms[index].hour = hour;
+    alarms[index].min = min;
+    alarms[index].alarmDelay = alarmDelay;
+}
+
+int indexOfHour(int hour)
+{
+    for (int i = 0; i < MAX_ALARMS; i++)
+    {
+        if (alarms[i].hour == hour)
+        {
+            return i; // Mengembalikan indeks alarm yang sesuai
+        }
+    }
+    return -1; // Mengembalikan -1 jika tidak ditemukan
+}
+
+bool buzzerState = true;
+void playAlarm(int gap)
+{
+    if (buzzerState)
+    {
+        myBuzzer.on();
+    }
+    else
+    {
+        myBuzzer.off();
+    }
+
+    buzzerState = !buzzerState;
+    delay(gap);
+}
+
+void checkAlarm(int hour, int minute)
+{
+    for (int i = 0; i < MAX_ALARMS; i++)
+    {
+        if ((alarms[i].hour == hour) && (alarms[i].min == minute))
+        {
+            Serial.print("\nAlert alarm ");
+            Serial.print(i);
+            Serial.print("\n");
+            playAlarm(alarms[i].alarmDelay);
+            return;
+        }
+    }
+    myBuzzer.off();
+}
