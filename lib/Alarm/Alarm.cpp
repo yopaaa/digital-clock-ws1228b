c08971dabf4fdd28194f96094724d2643ff83067
yopa <90709631+yopaaa@users.noreply.github.com>
2023-06-17 21:29:14 +0700

    add days to alarm

--- lib/Alarm/Alarm.cpp
+++ lib/Alarm/Alarm.cpp
@@ -4,6 +4,28 @@
 #include "Buzzer.h"
 
 Alarm alarms[MAX_ALARMS] = {};
+int day = 0x11;
+
+void hexToBinaryArray(unsigned int hexValue, int *binaryArray)
+{
+    for (int i = 7 - 1; i >= 0; i--)
+    {
+        binaryArray[i] = hexValue & 0x01;
+        hexValue >>= 1;
+    }
+}
+
+unsigned int binaryArrayToHex(const int *binaryArray)
+{
+    unsigned int hexValue = 0;
+
+    for (int i = 0; i < 7; i++)
+    {
+        hexValue |= binaryArray[i] << (7 - i - 1);
+    }
+
+    return hexValue;
+}
 
 // Fungsi untuk menulis data alarm ke EEPROM
 void writeAlarmsToEEPROM()
@@ -19,7 +41,9 @@ void writeAlarmsToEEPROM()
 
         EEPROM.write(address, alarms[i].hour);
         EEPROM.write(address + 1, alarms[i].min);
-        address += 2;
+        EEPROM.write(address + 2, alarms[i].alertIndex);
+        EEPROM.write(address + 3, binaryArrayToHex(alarms[i].days));
+        address += 4;
     }
     EEPROM.commit();
 }
@@ -38,12 +62,24 @@ void readAlarmsFromEEPROM()
 
         alarms[i].hour = EEPROM.read(address);
         alarms[i].min = EEPROM.read(address + 1);
-        address += 2;
+        alarms[i].alertIndex = EEPROM.read(address + 2);
+
+        int binaryArray[7] = {0};
+        hexToBinaryArray(EEPROM.read(address + 3), binaryArray);
+        alarms[i].days[0] = binaryArray[0];
+        alarms[i].days[1] = binaryArray[1];
+        alarms[i].days[2] = binaryArray[2];
+        alarms[i].days[3] = binaryArray[3];
+        alarms[i].days[4] = binaryArray[4];
+        alarms[i].days[5] = binaryArray[5];
+        alarms[i].days[6] = binaryArray[6];
+
+        address += 4;
     }
 }
 
 // Fungsi untuk mengatur alarm pada indeks tertentu
-void setAlarm(int index, int hour, int min)
+void setAlarm(int index, int days[7], int hour, int min, int alertIndex)
 {
     Serial.print("\nset alarms ");
     Serial.print(index);
@@ -52,18 +88,15 @@ void setAlarm(int index, int hour, int min)
 
     alarms[index].hour = hour;
     alarms[index].min = min;
-}
+    alarms[index].alertIndex = alertIndex;
 
-int indexOfHour(int hour)
-{
-    for (int i = 0; i < MAX_ALARMS; i++)
-    {
-        if (alarms[i].hour == hour)
-        {
-            return i; // Mengembalikan indeks alarm yang sesuai
-        }
-    }
-    return -1; // Mengembalikan -1 jika tidak ditemukan
+    alarms[index].days[0] = days[0];
+    alarms[index].days[1] = days[1];
+    alarms[index].days[2] = days[2];
+    alarms[index].days[3] = days[3];
+    alarms[index].days[4] = days[4];
+    alarms[index].days[5] = days[5];
+    alarms[index].days[6] = days[6];
 }
 
 bool buzzerState = true;
@@ -82,15 +115,19 @@ void playAlarm(int gap)
     delay(gap);
 }
 
-void checkAlarm(int hour, int minute)
+void checkAlarm(int day, int hour, int minute)
 {
     for (int i = 0; i < MAX_ALARMS; i++)
     {
-        if ((alarms[i].hour == hour) && (alarms[i].min == minute))
+        if ((alarms[i].hour == hour) && (alarms[i].min == minute) && (alarms[i].days[6 - day] == 1))
         {
-            Serial.print("\nAlert alarm ");
-            Serial.print(i);
-            playAlarm(0);
+            if (alarms[i].alertIndex == 1)
+            {
+                Serial.print("\nAlert alarm ");
+                Serial.print(i);
+                playAlarm(0);
+            }
+
             return;
         }
     }
