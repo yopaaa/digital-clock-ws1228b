bcd9500d13c124bb67e6f3063f2ed74d6da23bba
yopa <90709631+yopaaa@users.noreply.github.com>
2023-06-17 21:29:33 +0700

    add some code to handle alarm

--- lib/Alarm/Alarm.h
+++ lib/Alarm/Alarm.h
@@ -9,6 +9,8 @@ struct Alarm
 {
     int hour;
     int min;
+    int days[7];
+    int alertIndex;
 };
 
 // array to store alarm
@@ -16,7 +18,10 @@ extern Alarm alarms[MAX_ALARMS];
 extern int numAlarms;
 void readAlarmsFromEEPROM();
 void writeAlarmsToEEPROM();
-void checkAlarm(int hour, int minute);
-void setAlarm(int index, int hour, int min);
+void checkAlarm(int day, int hour, int minute);
+void setAlarm(int index, int days[7], int hour, int min, int alertIndex);
 void playAlarm(int gap);
+
+void hexToBinaryArray(unsigned int hexValue, int *binaryArray);
+unsigned int binaryArrayToHex(const int *binaryArray);
 #endif
