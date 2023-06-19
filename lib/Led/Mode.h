e013b1c9e3ce82d0b2944c5ac693377a05d34641
yopa <90709631+yopaaa@users.noreply.github.com>
2023-06-19 20:33:22 +0700

    Create Mode.h

---
+++ lib/Led/Mode.h
@@ -0,0 +1,33 @@
+#ifndef MODE_H
+#define MODE_H
+
+#include <Arduino.h>
+#include "ADDRESS.h"
+
+extern String segment1mode;
+extern String segment2mode;
+extern int interval;
+
+extern String Mode;
+extern int counterCount;
+extern int counterLimit;
+extern int countDownCount;
+
+extern int scors1;
+extern int scors2;
+
+extern int timeFormat;
+
+extern String ntpServer;
+extern long gmtOffset_sec; // UTC+7
+extern int daylightOffset_sec;
+
+void printCountDown();
+void printLocalTime();
+void printCounter();
+void printScors();
+void CLI();
+
+extern void (*mainLoop)();
+
+#endif
