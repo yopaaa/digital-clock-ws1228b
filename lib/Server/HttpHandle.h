14d55dfc0eb7b09e6d183e92792eb599b5e4608f
yopa <90709631+yopaaa@users.noreply.github.com>
2023-06-22 07:20:25 +0700

    Update HttpHandle.h

--- lib/Server/HttpHandle.h
+++ lib/Server/HttpHandle.h
@@ -4,6 +4,9 @@
 #include <Arduino.h>
 #include <ESPAsyncWebServer.h>
 #include <ArduinoJson.h>
+#include "Storage.h"
+#include "WifiFunc.h"
+
 #include "Buzzer.h"
 #include "ADDRESS.h"
 #include "Led.h"
@@ -17,7 +20,12 @@ struct Route
     void (*handle)(AsyncWebServerRequest *, DynamicJsonDocument &, DynamicJsonDocument &);
 };
 
-void HandleSetColor(AsyncWebServerRequest *request, DynamicJsonDocument &jsonDoc, DynamicJsonDocument &json);
-void HandleSetMode(AsyncWebServerRequest *request, DynamicJsonDocument &jsonDoc, DynamicJsonDocument &json);
+extern Route routes[];
+extern int numRoutes;
+void HandleWifi(AsyncWebServerRequest *request, DynamicJsonDocument &jsonDoc, DynamicJsonDocument &json);
+void HandleColor(AsyncWebServerRequest *request, DynamicJsonDocument &jsonDoc, DynamicJsonDocument &json);
+void HandleMode(AsyncWebServerRequest *request, DynamicJsonDocument &jsonDoc, DynamicJsonDocument &json);
+void HandleTime(AsyncWebServerRequest *request, DynamicJsonDocument &jsonDoc, DynamicJsonDocument &json);
+void HandleInfo(AsyncWebServerRequest *request, DynamicJsonDocument &jsonDoc, DynamicJsonDocument &json);
 
 #endif
