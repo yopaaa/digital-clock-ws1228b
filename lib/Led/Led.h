353b3e7e4a1e394d1eb0fc9702fc1aadc312edcc
yopa <90709631+yopaaa@users.noreply.github.com>
2023-07-07 11:33:17 +0700

    Update Led.h

--- lib/Led/Led.h
+++ lib/Led/Led.h
@@ -4,20 +4,22 @@
 #include <Arduino.h>
 #include "ADDRESS.h"
 
-#define CODE_VERSION "v3.6.21" // tahun 202'3', bulan .., tanggal ..
+extern String CODE_VERSION;
 #define NUMBER_SEGMENT 4
 #define LEDS_PER_SEGMENT 7
 #define NUM_LEDS 28
 #define NUM_DOTS 2
 
 #if defined(ESP8266)
-#define LED_PIN 0
-#define WIFI_AP_BTN 4
-#define BUZZER_PIN 2
+#define LED_PIN 16
+#define DOTS_PIN 14
+#define WIFI_AP_BTN 12
+#define BUZZER_PIN 13
 #elif defined(ESP32)
 #define LED_PIN 23
+#define DOTS_PIN 22
 #define WIFI_AP_BTN 4
-#define BUZZER_PIN 22
+#define BUZZER_PIN 21
 #endif
 
 extern byte RED;
@@ -40,7 +42,5 @@ void ErrorDisplay(int one, int two);
 void SetNumber(int segment, int num);
 void PrintNumber(int num, String segment);
 void SetBrightness(byte val);
-
-void setMode(String mode);
 
 #endif
