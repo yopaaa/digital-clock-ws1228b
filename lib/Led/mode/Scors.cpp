7332a468d95775756fad2d24befe3ca656829866
yopa <90709631+yopaaa@users.noreply.github.com>
2023-07-12 18:35:54 +0700

    update scors to non blocking code with millis

--- lib/Led/mode/Scors.cpp
+++ lib/Led/mode/Scors.cpp
@@ -1,12 +1,18 @@
 #include "Led.h"
 #include "Mode.h"
 
+unsigned long previousMilliss = 0;
+
 void printScors()
 {
-    BlinkDots();
-    PrintNumber(scors1, "left");
-    PrintNumber(scors2, "right");
-    delay(interval);
+    unsigned long currentMillis = millis();
+
+    if (currentMillis - previousMilliss >= interval)
+    {
+        previousMilliss = currentMillis;
+
+        BlinkDots();
+        PrintNumber(scors1, "left");
+        PrintNumber(scors2, "right");
+    }
 }
-
-
